3f2cc00c8d43ed42358a143dbdb158a620114a57
Hagar Ahmed <85418088+Hagar123-cell@users.noreply.github.com>
2022-04-26 03:57:46 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,22 +1,26 @@
 /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package newpackage;
+package com.mycompany.first;
 
 /**
  *
  * @author Habiba
  */
 public class Client extends User {
-    protected int Balance;
+    protected float Balance;
     protected String Address;
     protected int PhNo;
     protected char AccNo;
     protected int TransferredMoney;
-    protected int PrevTrans;
-    public Client(String N,int Age,int Id,int B,String A,int P,char Acc){
+    protected float PrevTrans;
+    public Client(String N,int Age,int Id,float B,String A,int P,char Acc){
         Name=N;
         this.Age=Age;
         this.Id=Id;
@@ -25,7 +29,7 @@ public class Client extends User {
         PhNo =P;
         AccNo=Acc;
     }
-    public void setBalance(int Balance) {
+    public void setBalance(float Balance) {
         this.Balance = Balance;
     }
 
@@ -57,12 +61,25 @@ public class Client extends User {
         return AccNo;
     }
 
+    public void get_loan(float amount)
+    {
+        if(amount <= Balance )
+        {
+                    System.out.println("loan is accepted");
+                    Balance= Balance + amount;
+        }
+                    
+        else
+                    System.out.println("loan is rejected");
+
+
+    }
     public int getTransferredMoney() {
         return TransferredMoney;
     }
     
    
-    public void DepositeMoney(int Money){
+    public void DepositeMoney(float Money){
         if(Money!=0){
             Balance+=Money;
             PrevTrans=Money;
@@ -74,13 +91,13 @@ public class Client extends User {
             PrevTrans=Money;
         } 
     }
-    public int CheckBalance(){
+    public float CheckBalance(){
         return Balance;
     }
     public void TransferMoney(int Money){
         TransferredMoney =Money;
     }
-    public int getPrevTrans() {
+    public float getPrevTrans() {
         if(PrevTrans>0)
             return PrevTrans;
         else if(PrevTrans<0)
